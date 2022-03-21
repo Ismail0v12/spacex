75ac6d117569634b7c394b99ce9f3d48d74b0026
Ismail0v12 <orifisamilov12@mail.ru>
2022-03-21 23:10:08 +0500

    Enabled dnd on mobile devices

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 import {DndProvider} from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend';
+import {TouchBackend} from "react-dnd-touch-backend";
+import {HTML5Backend} from "react-dnd-html5-backend";
 import "react-loading-skeleton/dist/skeleton.css";
 
 import App from "./components/app/app";
 
+const options = {
+  enableMouseEvents: true
+}
+
+const {innerWidth: width} = window;
+
+const renderBackend = width > 992 ? HTML5Backend : TouchBackend;
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={renderBackend} options={options}>
         <App/>
       </DndProvider>
     </Router>
